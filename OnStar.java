29e492a46dcdbb93153c9c36ba0c3cd2be6254c2
Derrick Stone <djs6d@virginia.edu>
2024-09-01 11:54:04 -0400

    adding java file

---
+++ OnStar.java
@@ -0,0 +1,24 @@
+public class OnStar {
+
+    public String greeting() {
+        return "Hello, this is OnStar. How may I help you?";
+    }
+    public double calculateMileage(int miles, double fuelConsumed) {
+        // determine miles per gallon
+
+        if (fuelConsumed==0) {
+            return 0.0;
+        }
+
+        return miles / fuelConsumed;
+
+    }
+    public double calculateAverageSpeed(double[] speed) {
+        // return an average miles per hour using a number of samples
+        double averageSpeed = 0.0;
+        for ( int i=0;i<speed.length;i++) {
+            averageSpeed+=speed[i];
+        }
+        return averageSpeed/speed.length;
+    }
+}
